2b49e2b341382ca79938ae24fc3d67f7b8097aed
1 <1@DESKTOP-DJUITLQ>
2023-06-28 23:46:42 +0300

    Add project files.

---
+++ intriductionToOOP/Source.cpp
@@ -0,0 +1,174 @@
+#include <iostream>
+using namespace std;
+
+class Fraction
+{
+	int integer;//целая часть
+	int numerator;//числитель
+	int denominator;//знаменатель
+public:
+	int get_integer()const
+	{
+		return integer;
+	}
+	int get_numerator()const
+	{
+		return numerator;
+	}
+	int get_denominator()const
+	{
+		return denominator;
+	}
+	void set_integer(int integer)
+	{
+		this->integer = integer;
+	}
+	void set_numerator(int numerator)
+	{
+		this->numerator = numerator;
+	}
+	void set_denominator(int denominator)
+	{
+		if (denominator == 0)denominator = 1;
+		this->denominator = denominator;
+	}
+
+	//Constructors
+
+	Fraction()
+	{
+		this->integer = 0;
+		this->numerator = 0;
+		this->denominator = 1;
+		cout << "DefaultConstruct:/t" << this << endl;
+	}
+	Fraction(int integer)
+	{
+		this->integer = integer;
+		this->numerator = 0;
+		this->denominator = 1;
+		cout << "1ArgConstructor:/t" << this << endl;
+	}
+	Fraction(int numerator, int denominator)
+	{
+		this->integer = 0;
+		this->numerator = numerator;
+		set_denominator(denominator);
+		cout << "Constructor:/t" << this << endl;
+	}
+
+	Fraction(int integer, int numerator, int denominator)
+	{
+		this->integer = integer;
+		this->numerator = numerator;
+		set_denominator(denominator);
+		cout << "Constructors:\t" << this << endl;
+	}
+	Fraction(const Fraction& other)
+	{
+		this->integer = other.integer;
+		this->numerator = other.numerator;
+		this->denominator = other.denominator;
+		cout << "CopyComstructor:/t" << this << endl;
+	}
+	~Fraction()
+	{
+		cout << "Destructor:\t" << this << endl;
+	}
+	//Operators
+	Fraction& operator=(const Fraction& other)
+	{
+		this->integer = other.integer;
+		this->numerator = other.numerator;
+		this->denominator = other.denominator;
+		cout << "CopyAssgment:/t" << this << endl;
+		return *this;
+	}
+
+	//Methods
+	Fraction& to_improper()
+	{
+		numerator += integer * denominator;
+		integer = 0;
+		return *this;
+	}
+	Fraction& to_proper()
+	{
+		integer += numerator / denominator;
+		numerator %= denominator;
+		return *this;
+	}
+	Fraction inverted()const
+	{
+		Fraction inverted = *this;
+		inverted.to_improper();
+		std::swap(inverted.numerator, inverted.denominator);
+		return inverted;
+	}
+	void print()const
+	{
+		if (integer)cout << integer;//если есть целая часть выводим ее на экран
+		if (numerator)
+		{
+			if (integer)cout << "(";
+			cout << numerator << "/" << denominator;
+			if (integer)cout << ")";
+		}
+		else if (integer == 0)cout << 0;
+		cout << endl;
+	}
+
+
+};
+
+Fraction operator*(Fraction left, Fraction right)
+{
+	left.to_improper();
+	right.to_improper();
+	return Fraction
+	{
+		left.get_numerator() * right.get_numerator(),
+		left.get_denominator() * right.get_denominator(),
+
+	}. to_proper();
+};
+
+Fraction operator/(const Fraction& left, const Fraction& right)
+{
+	return left * right.inverted();
+}
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+#ifdef CONSTRUCTORS_CHECK
+	Fraction A;//Def constructor
+	A.print();
+
+	Fraction B = 5;
+	B.print();
+
+	Fraction C(1, 2);
+	C.print();
+
+	Fraction D(2, 3, 4);
+	D.print();
+
+	Fraction E = C;//Copy constructor
+	E.print();
+
+	Fraction F;
+	F = D;
+	F.print();
+#endif CONSTRUCTORS_CHECK
+
+	Fraction A(2, 3, 4);
+	A.print();
+
+	Fraction B(3, 4, 5);
+	B.print();
+	Fraction C = A * B;
+	C.print();
+
+	Fraction D = A / B;
+	D.print();
+}
